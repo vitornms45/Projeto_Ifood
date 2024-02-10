b23d9a4790518a2b3d35aba2b6b1eeefd26593db
Victor Nunes Moraes Silva <vitornones25@gmail.com>
2024-02-10 19:50:43 -0300

    Database refatorada.

--- Ifood_DB_CreateRegister.sql
+++ Ifood_DB_CreateRegister.sql
@@ -8,20 +8,28 @@ num_endereco    varchar(6)         not null,
 complemento     varchar(80)        null,
 senha           varchar(80)        not null
 
-CONSTRAINT PK_CADASTRO_DB_Email_Id primary key clustered (Email, id)
+CONSTRAINT PK_CADASTRO_DB_Email_Id primary key clustered (Email)
 )
 
 insert into CADASTRO_DB(Email,Telefone,Nome,endereco,num_endereco,complemento,senha)
 values(
-'Teste@testemail.com.br','11932427543','Teste da silva junior','Rua das campinas','35','B','Testedesenha123'
+'Teste@testemail.com.br',
+'11932427543'			,
+'Teste da silva junior'	,
+'Rua das campinas'		,
+'35'					,
+'B'						,
+'Testedesenha123'
 )
 
 insert into CADASTRO_DB(Email,Telefone,Nome,endereco,num_endereco,senha)
 values(
-'Teste@testemail.com.br'		,
+'novo@testemail.com.br'			,
 '11932427543'					,
 'Teste da silva junior'			,
 'Rua das campinas'				,
 '35'							,
 'Testedesenha123'
 )
+
+select * from CADASTRO_DB 

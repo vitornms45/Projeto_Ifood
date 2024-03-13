f6457d58310322fa4c230cfb0ec00cbe9684cf50
Victor Nunes Moraes Silva <vitornones25@gmail.com>
2024-03-13 17:39:16 -0300

    features

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,4 @@
-'''from flask import Flask
+from flask import Flask
 app = Flask(__name__)
-from Projeto_Ifood import routes
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)'''
+from app import routes
